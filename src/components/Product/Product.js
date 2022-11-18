527ed0daec1876881e6e3bc5ca0780f60af68f63
Julia Diaków <tagitsu@gmail.com>
2022-11-18 19:26:16 +0100

    Change Products component

---
+++ src/components/Product/Product.js
@@ -0,0 +1,54 @@
+import styles from './Product.module.scss';
+import clsx from 'clsx';
+import Button from '../Button/Button';
+import PropTypes from 'prop-types';
+
+const Product = props => {
+  return (
+    <article className={styles.product}>
+      <div className={styles.imageContainer}>
+        <img 
+          className={styles.image}
+          alt="Kodilla shirt"
+          src={`${process.env.PUBLIC_URL}/images/products/shirt-kodilla--black.jpg`} />
+      </div>
+      <div>
+        <header>
+          <h2 className={styles.name}>K</h2>
+          <span className={styles.price}>Price: 20$</span>
+        </header>
+        <form>
+          <div className={styles.sizes}>
+            <h3 className={styles.optionLabel}>Sizes</h3>
+            <ul className={styles.choices}>
+              <li><button type="button" className={styles.active}>S</button></li>
+              <li><button type="button">M</button></li>
+              <li><button type="button">L</button></li>
+              <li><button type="button">XL</button></li>
+            </ul>
+          </div>
+          <div className={styles.colors}>
+            <h3 className={styles.optionLabel}>Colors</h3>
+            <ul className={styles.choices}>
+              <li><button type="button" className={clsx(styles.colorBlack, styles.active)} /></li>
+              <li><button type="button" className={clsx(styles.colorRed)} /></li>
+              <li><button type="button" className={clsx(styles.colorWhite)} /></li>
+            </ul>
+          </div>
+          <Button className={styles.button}>
+            <span className="fa fa-shopping-cart" />
+          </Button>
+        </form>
+      </div>
+    </article>
+  )
+};
+Product.propTypes = {
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  basePrice: PropTypes.number.isRequired,
+  colors: PropTypes.array.isRequired,
+  sizes: PropTypes.array.isRequired
+};
+export default Product;
