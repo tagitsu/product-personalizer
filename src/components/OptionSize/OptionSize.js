17389d9c79ce9cd6edf4c02fc8ef08b6eb0688fd
Julia Diaków <tagitsu@gmail.com>
2022-11-21 14:27:11 +0100

    Fix according to comments

--- src/components/OptionSize/OptionSize.js
+++ src/components/OptionSize/OptionSize.js
@@ -2,8 +2,7 @@ import styles from '../ProductForm/ProductForm.module.scss';
 import shortid from 'shortid';
 import clsx from 'clsx';
 
-const OptionSize = props => {
-  return(
+const OptionSize = props => (
     <div className={styles.sizes}>
       <h3 className={styles.optionLabel}>Sizes</h3>
       <ul className={styles.choices}>
@@ -11,6 +10,5 @@ const OptionSize = props => {
       </ul>
     </div>
   );
-};
 
 export default OptionSize;
